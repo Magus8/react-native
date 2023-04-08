9155999eb8d8d212c3448a78c578eda5fcfc7ff3
Brian Beckwith <bbeckwith@outlook.com>
2023-04-08 14:02:32 -0400

    Workshop Assignment 3

---
+++ screens/oldModalInfo.js
@@ -0,0 +1,53 @@
+                
+                {/* <Modal 
+                    animationType='slide'
+                    transparent={false}
+                    visible={showModal}
+                    onRequestClose={() => setShowModal(!showModal)}    
+                >
+                    <View style={styles.modal}>
+                        <Text style={styles.modalTitle}>
+                            Search Campsite Reservations
+                        </Text>
+                        <Text style={styles.modalText}>
+                            Number of Campers: {campers}
+                        </Text>
+                        <Text style={styles.modalText}>
+                            Hike-In?: {hikeIn ? 'Yes' : 'No'}
+                        </Text>
+                        <Text style={styles.modalText}>
+                            Date: {date.toLocaleDateString('en-US')}
+                        </Text>
+                        <Button 
+                            onPress={() => {
+                                setShowModal(!showModal);
+                                resetForm();
+                            }}
+                            color='#5637DD'
+                            title='Close'
+                        />
+                    </View>
+                </Modal> */}
+
+                // const [showModal, setShowModal] = useState(false);
+                // const handleReservation = () => {
+                //     console.log({campers, hikeIn, date});
+                //     setShowModal(!showModal);
+                // };
+    //             ,
+    // modal: {
+    //     justifyContent: 'center',
+    //     margin: 20
+    // },
+    // modalTitle: {
+    //     fontSize: 24,
+    //     fontWeight: 'bold',
+    //     backgroundColor: '#5637DD',
+    //     textAlign: 'center',
+    //     color: '#fff',
+    //     marginBottom: 20
+    // },
+    // modalText: {
+    //     fontSize: 18,
+    //     margin: 10
+    // }
