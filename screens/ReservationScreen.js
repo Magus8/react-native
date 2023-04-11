cdbebb421e6ef867d766ceec38443243463277b2
Brian Beckwith <bbeckwith@outlook.com>
2023-04-10 20:01:01 -0400

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet, Switch, Button, Alert} from 'react-
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
 
@@ -33,7 +34,12 @@ const ReservationScreen = () => {
                 },
                 {
                     text: 'OK',
-                    onPress: () => resetForm()
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );    
+                        resetForm();
+                    }
                 }
             ],
             { cancelable: false }
@@ -46,6 +52,35 @@ const ReservationScreen = () => {
         setDate(new Date());
         setShowCalendar(false);
     };
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBade: true
+                })
+            });
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your campsite Reservation Search', 
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
+
     
     return (
         <ScrollView>
